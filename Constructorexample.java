dd32f638e0810f165171730f9c3177d678cb2158
Rohith Gadde <50209060+RohithGadde@users.noreply.github.com>
2020-08-15 21:44:06 +0530

    Add files via upload

---
+++ Constructorexample.java
@@ -0,0 +1,15 @@
+class Constructorexample
+{
+ public static void main(String[] args)
+ {
+  new Constructor();
+ }
+}
+
+class Constructor
+{
+ Constructor()
+ {
+  System.out.println("This is automatically called as it is a constructor");
+ }
+}
